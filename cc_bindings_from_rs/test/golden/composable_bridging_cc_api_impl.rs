51befb759b28bb359147c92c8b63e7072f458d93
Googler <no-reply@google.com>
2025-10-06 23:20:42 -0700

    G3 internal
    PiperOrigin-RevId: 816055949

--- cc_bindings_from_rs/test/golden/composable_bridging_cc_api_impl.rs
+++ cc_bindings_from_rs/test/golden/composable_bridging_cc_api_impl.rs
@@ -4,12 +4,25 @@
 
 // Automatically @generated C++ bindings for the following Rust crate:
 // composable_bridging_rust_golden
-// Features: custom_ffi_types, experimental, non_unpin_ctor, std_unique_ptr, std_vector, supported, wrapper
+// Features: assume_lifetimes, custom_ffi_types, experimental, non_unpin_ctor, std_unique_ptr, std_vector, supported, wrapper
 
 #![allow(unused_unsafe, deprecated, non_snake_case, unreachable_code)]
 #![allow(improper_ctypes_definitions)]
 #![deny(warnings)]
 
+#[unsafe(no_mangle)]
+unsafe extern "C" fn __crubit_thunk_maybe_uint_uslice(__ret_ptr: *mut core::ffi::c_uchar) -> () {
+    unsafe {
+        let __rs_return_value = ::composable_bridging_rust_golden::maybe_int_slice();
+        unsafe {
+            ::bridge_rust::internal::encode(
+                ::bridge_rust::OptionAbi(::bridge_rust::transmute_abi::<*const [i32]>()),
+                __ret_ptr as *mut core::ffi::c_uchar,
+                __rs_return_value,
+            );
+        }
+    }
+}
 #[unsafe(no_mangle)]
 unsafe extern "C" fn __crubit_thunk_returns_usome_uint(__ret_ptr: *mut core::ffi::c_uchar) -> () {
     unsafe {
@@ -36,9 +49,18 @@ unsafe extern "C" fn __crubit_thunk_unwrap_uor_uzero(x: *const core::ffi::c_ucha
     }
 }
 #[unsafe(no_mangle)]
-unsafe extern "C" fn __crubit_thunk_returns_uno_uint(__ret_ptr: *mut core::ffi::c_uchar) -> () {
+unsafe extern "C" fn __crubit_thunk_option_uincrements(
+    x: *const core::ffi::c_uchar,
+    __ret_ptr: *mut core::ffi::c_uchar,
+) -> () {
     unsafe {
-        let __rs_return_value = ::composable_bridging_rust_golden::returns_no_int();
+        let x = unsafe {
+            ::bridge_rust::internal::decode(
+                ::bridge_rust::OptionAbi(::bridge_rust::transmute_abi::<i32>()),
+                x,
+            )
+        };
+        let __rs_return_value = ::composable_bridging_rust_golden::option_increments(x);
         unsafe {
             ::bridge_rust::internal::encode(
                 ::bridge_rust::OptionAbi(::bridge_rust::transmute_abi::<i32>()),
@@ -49,12 +71,12 @@ unsafe extern "C" fn __crubit_thunk_returns_uno_uint(__ret_ptr: *mut core::ffi::
     }
 }
 #[unsafe(no_mangle)]
-unsafe extern "C" fn __crubit_thunk_maybe_uint_uslice(__ret_ptr: *mut core::ffi::c_uchar) -> () {
+unsafe extern "C" fn __crubit_thunk_returns_uno_uint(__ret_ptr: *mut core::ffi::c_uchar) -> () {
     unsafe {
-        let __rs_return_value = ::composable_bridging_rust_golden::maybe_int_slice();
+        let __rs_return_value = ::composable_bridging_rust_golden::returns_no_int();
         unsafe {
             ::bridge_rust::internal::encode(
-                ::bridge_rust::OptionAbi(::bridge_rust::transmute_abi::<*const [i32]>()),
+                ::bridge_rust::OptionAbi(::bridge_rust::transmute_abi::<i32>()),
                 __ret_ptr as *mut core::ffi::c_uchar,
                 __rs_return_value,
             );
@@ -72,28 +94,6 @@ unsafe extern "C" fn __crubit_thunk_make_umy_uoption_urust(
                 ::composable_bridging_rust_golden::MyOptionRustAbi(::bridge_rust::transmute_abi::<
                     i32,
                 >()),
-                __ret_ptr as *mut core::ffi::c_uchar,
-                __rs_return_value,
-            );
-        }
-    }
-}
-#[unsafe(no_mangle)]
-unsafe extern "C" fn __crubit_thunk_option_uincrements(
-    x: *const core::ffi::c_uchar,
-    __ret_ptr: *mut core::ffi::c_uchar,
-) -> () {
-    unsafe {
-        let x = unsafe {
-            ::bridge_rust::internal::decode(
-                ::bridge_rust::OptionAbi(::bridge_rust::transmute_abi::<i32>()),
-                x,
-            )
-        };
-        let __rs_return_value = ::composable_bridging_rust_golden::option_increments(x);
-        unsafe {
-            ::bridge_rust::internal::encode(
-                ::bridge_rust::OptionAbi(::bridge_rust::transmute_abi::<i32>()),
                 __ret_ptr as *mut core::ffi::c_uchar,
                 __rs_return_value,
             );
--- rs_bindings_from_cc/generate_bindings/generate_function.rs
+++ rs_bindings_from_cc/generate_bindings/generate_function.rs
@@ -1543,8 +1543,8 @@ pub fn generate_function(
         // If we don't have an outer `impl ... { ... }` block, we have to introduce the
         // lifetimes and bounds inside this one.
         let has_wrapping_impl = match impl_kind {
-            ImplKind::None { .. } => false,
-            ImplKind::Struct { .. } | ImplKind::Trait { .. } => true,
+            ImplKind::None { .. } | ImplKind::Struct { .. } => false,
+            ImplKind::Trait { .. } => true,
         };
         let where_clause = if has_wrapping_impl {
             None
@@ -1610,6 +1610,7 @@ pub fn generate_function(
         generate_doc_comment(func.doc_comment.as_deref(), Some(&func.source_loc), db.environment());
     let api_func: TokenStream;
     let function_id: FunctionId;
+    let mut member_functions_map = HashMap::new();
     match impl_kind {
         ImplKind::None { .. } => {
             api_func = quote! { #unimplemented_trait_def #doc_comment #api_func_def };
@@ -1622,15 +1623,12 @@ pub fn generate_function(
             let record_name = make_rs_ident(
                 derived_record.as_deref().unwrap_or(record.as_ref()).rs_name.identifier.as_ref(),
             );
-            let error_lifetime_generic =
-                error_lifetime_param.as_ref().map(|lifetime| quote! { <#lifetime> });
+            member_functions_map.insert(
+                derived_record.as_deref().unwrap_or(record.as_ref()).id,
+                vec![quote! { #unsatisfied_where_clause #doc_comment #api_func_def }],
+            );
             api_func = quote! {
                 #unimplemented_trait_def
-                impl #error_lifetime_generic #record_name
-                #unsatisfied_where_clause {
-                    #doc_comment
-                    #api_func_def
-                }
             };
             function_id = FunctionId {
                 self_type: None,
@@ -1815,6 +1813,7 @@ pub fn generate_function(
         },
         features,
         cc_details,
+        member_functions: member_functions_map,
         ..Default::default()
     };
 
