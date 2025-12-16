5c9b0f43baba3cd508f61729ca4e97105ae7b2a7
Googler <no-reply@google.com>
2025-12-15 22:24:02 -0800

    G3 internal
    PiperOrigin-RevId: 845084263

--- cc_bindings_from_rs/test/function_pointers/function_pointers_cc_api_impl.rs
+++ cc_bindings_from_rs/test/function_pointers/function_pointers_cc_api_impl.rs
@@ -4,29 +4,12 @@
 
 // Automatically @generated C++ bindings for the following Rust crate:
 // function_pointers_golden
-// Features: custom_ffi_types, experimental, non_unpin_ctor, std_unique_ptr, std_vector, supported, wrapper
+// Features: assume_lifetimes, custom_ffi_types, experimental, non_unpin_ctor, std_unique_ptr, std_vector, supported, wrapper
 
 #![allow(unused_unsafe, deprecated, non_snake_case, unreachable_code)]
 #![allow(improper_ctypes_definitions)]
 #![deny(warnings)]
 
-#[unsafe(no_mangle)]
-unsafe extern "C" fn __crubit_thunk_call_ufn_uptr_uwith_ufive(
-    fn_ptr: extern "C" fn(i32) -> i32,
-) -> i32 {
-    unsafe { ::function_pointers_golden::call_fn_ptr_with_five(fn_ptr) }
-}
-const _: () = assert!(::std::mem::size_of::<::function_pointers_golden::CStruct>() == 4);
-const _: () = assert!(::std::mem::align_of::<::function_pointers_golden::CStruct>() == 4);
-#[unsafe(no_mangle)]
-unsafe extern "C" fn __crubit_thunk_default(__ret_ptr: *mut core::ffi::c_void) -> () {
-    unsafe {
-        let __rs_return_value =
-            <::function_pointers_golden::CStruct as ::core::default::Default>::default();
-        (__ret_ptr as *mut ::function_pointers_golden::CStruct).write(__rs_return_value);
-    }
-}
-const _: () = assert!(::core::mem::offset_of!(::function_pointers_golden::CStruct, field) == 0);
 #[unsafe(no_mangle)]
 unsafe extern "C" fn __crubit_thunk_call_ufn_uptr_uno_uargs_uor_ureturn(
     fn_ptr: extern "C" fn(),
@@ -51,3 +34,20 @@ unsafe extern "C" fn __crubit_thunk_with_uadd_uten(__ret_ptr: *mut core::ffi::c_
     }
 }
 const _: () = assert!(::core::mem::offset_of!(::function_pointers_golden::HasFnPtrField, ptr) == 0);
+const _: () = assert!(::std::mem::size_of::<::function_pointers_golden::CStruct>() == 4);
+const _: () = assert!(::std::mem::align_of::<::function_pointers_golden::CStruct>() == 4);
+#[unsafe(no_mangle)]
+unsafe extern "C" fn __crubit_thunk_default(__ret_ptr: *mut core::ffi::c_void) -> () {
+    unsafe {
+        let __rs_return_value =
+            <::function_pointers_golden::CStruct as ::core::default::Default>::default();
+        (__ret_ptr as *mut ::function_pointers_golden::CStruct).write(__rs_return_value);
+    }
+}
+const _: () = assert!(::core::mem::offset_of!(::function_pointers_golden::CStruct, field) == 0);
+#[unsafe(no_mangle)]
+unsafe extern "C" fn __crubit_thunk_call_ufn_uptr_uwith_ufive(
+    fn_ptr: extern "C" fn(i32) -> i32,
+) -> i32 {
+    unsafe { ::function_pointers_golden::call_fn_ptr_with_five(fn_ptr) }
+}
--- cc_bindings_from_rs/test/golden/non_trivially_destructible_cc_api_impl.rs
+++ cc_bindings_from_rs/test/golden/non_trivially_destructible_cc_api_impl.rs
@@ -4,20 +4,12 @@
 
 // Automatically @generated C++ bindings for the following Rust crate:
 // non_trivially_destructible_rust_golden
-// Features: custom_ffi_types, experimental, non_unpin_ctor, std_unique_ptr, std_vector, supported, wrapper
+// Features: assume_lifetimes, custom_ffi_types, experimental, non_unpin_ctor, std_unique_ptr, std_vector, supported, wrapper
 
 #![allow(unused_unsafe, deprecated, non_snake_case, unreachable_code)]
 #![allow(improper_ctypes_definitions)]
 #![deny(warnings)]
 
-#[unsafe(no_mangle)]
-unsafe extern "C" fn __crubit_thunk_return_uby_uvalue(__ret_ptr: *mut core::ffi::c_void) -> () {
-    unsafe {
-        let __rs_return_value = ::non_trivially_destructible_rust_golden::return_by_value();
-        (__ret_ptr as *mut ::non_trivially_destructible_rust_golden::NonTriviallyDestructable)
-            .write(__rs_return_value);
-    }
-}
 const _: () = assert!(
     ::std::mem::size_of::<::non_trivially_destructible_rust_golden::NonTriviallyDestructable>()
         == 4
@@ -68,6 +60,14 @@ const _: () = assert!(
         field
     ) == 0
 );
+#[unsafe(no_mangle)]
+unsafe extern "C" fn __crubit_thunk_return_uby_uvalue(__ret_ptr: *mut core::ffi::c_void) -> () {
+    unsafe {
+        let __rs_return_value = ::non_trivially_destructible_rust_golden::return_by_value();
+        (__ret_ptr as *mut ::non_trivially_destructible_rust_golden::NonTriviallyDestructable)
+            .write(__rs_return_value);
+    }
+}
 #[unsafe(no_mangle)]
 unsafe extern "C" fn __crubit_thunk_take_uby_uvalue(
     _x: &'static mut ::core::mem::MaybeUninit<
